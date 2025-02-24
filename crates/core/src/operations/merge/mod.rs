082f9a837d03c9c0b171203c278fe48fae94e513
Andrew Lamb <andrew@nerdnetworks.org>
2025-02-24 13:05:48 -0500

    chore: Update to DataFusion 46.0.0, update for API chanages
    Signed-off-by: Andrew Lamb <andrew@nerdnetworks.org>

--- crates/core/src/operations/merge/mod.rs
+++ crates/core/src/operations/merge/mod.rs
@@ -27,7 +27,6 @@
 //!     })?
 //!     .await?
 //! ````
-
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::ops::Deref;
@@ -540,18 +539,22 @@ impl MergeOperation {
                         Column {
                             relation: None,
                             name,
+                            spans,
                         } => Column {
                             relation: Some(r),
                             name,
+                            spans,
                         },
                         Column {
                             relation: Some(TableReference::Bare { table }),
                             name,
+                            spans,
                         } => {
                             if table.as_ref() == alias {
                                 Column {
                                     relation: Some(r),
                                     name,
+                                    spans,
                                 }
                             } else {
                                 return Err(DeltaTableError::Generic(
